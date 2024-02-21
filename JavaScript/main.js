588235ee70755b5a208f145cf6660aff73faa703
Mosab-Mohmead-Srmini <abnt79957@gmail.com>
2024-02-22 02:58:19 +0300

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,42 @@
+const startGame=document.getElementById("startGame");
+const startWatch=document.getElementById("startWatch");
+startGame.addEventListener("click",stGame);
+function stGame(){
+    window.location.href="http://127.0.0.1:5500/Html/game.html";
+}
+
+startWatch.addEventListener("click",stWatch);
+function stWatch(){
+    window.location.href="https://www.youtube.com/watch?v=aW9npy25W7A";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
